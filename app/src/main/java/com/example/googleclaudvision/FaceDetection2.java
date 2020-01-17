6e55d7aeaf36753fb64a3dd59b5149b97286e91c
MariaFernandezC <luzmaria26tkm@gmail.com>
2020-01-17 18:50:41 -0500

    tarea

--- app/src/main/java/com/example/googleclaudvision/FaceDetection2.java
+++ app/src/main/java/com/example/googleclaudvision/FaceDetection2.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.services.vision.v1.Vision;
@@ -69,7 +77,10 @@ public class FaceDetection2 extends AppCompatActivity {
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 90, stream);
                 byte[] imageInByte = stream.toByteArray();
 
-
+                final Paint boxPaint =new Paint();
+                boxPaint.setStrokeWidth(5);
+                boxPaint.setColor(Color.GREEN);
+                boxPaint.setStyle(Paint.Style.STROKE);
                 Image inputImage = new Image();
                 inputImage.encodeContent(imageInByte);
 
@@ -93,8 +104,14 @@ public class FaceDetection2 extends AppCompatActivity {
                     //5 enviamos la solicitud
                     annotateRequest.setDisableGZipContent(true);
                     BatchAnnotateImagesResponse batchResponse  = annotateRequest.execute();
-
-
+                    final Bitmap tempBitmap = Bitmap.createBitmap(bitmap.getWidth(),bitmap.getHeight(),Bitmap.Config.RGB_565);
+                    final Canvas canvas =new Canvas(tempBitmap);
+                    canvas.drawBitmap(bitmap,0,0,null);
+                    FaceDetector faceDetector =new FaceDetector.Builder(getApplicationContext())
+                            .setTrackingEnabled(false)
+                            .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                            .setMode(FaceDetector.FAST_MODE)
+                            .build();
                     List<FaceAnnotation> faces = batchResponse .getResponses()
                             .get(0).getFaceAnnotations();
                     int numberOfFaces = faces.size();
@@ -102,6 +119,21 @@ public class FaceDetection2 extends AppCompatActivity {
                     for(int i=0; i<numberOfFaces; i++) {
                         likelihoods += "\n It is " +  faces.get(i).getJoyLikelihood() +
                                 " that face " + i + " is happy";
+
+                        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                        SparseArray<Face> sparseArray =faceDetector.detect(frame);
+
+                        for (int j=0; j<sparseArray.size();j++){
+
+                            Face face =sparseArray.valueAt(j);
+                            float x1=face.getPosition().x;
+                            float y1=face.getPosition().y;
+                            float x2 =x1+face.getWidth();
+                            float y2 =y1+face.getHeight();
+                            RectF rectF = new RectF(x1,y1,x2,y2);
+                            canvas.drawRoundRect(rectF,2,2,boxPaint);
+                        }
+                        imageView.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
                     }
                     final String message =
                             "This photo has " + numberOfFaces + " faces" + likelihoods;
